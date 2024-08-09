86d9c2ba1cd54395052ce859155f7558b5f87bc3
maru <poprockguitarmaru@gmail.com>
2024-08-09 21:48:14 +0900

    議会報告追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # PDFファイルのサムネイル画像のパスを取得
+    @pdf_thumbnails = Dir.glob(Rails.root.join('public', 'advertisement', 'thumbnails', '*_page_1.png'))
   end
 end
